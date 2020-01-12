baa71063531c1f147affd5dbe17c7616e091722c
eugeny-m <evgenii.maksimov@yahoo.com>
2020-01-12 16:50:15 +0300

    add input file type validation

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,30 @@
+import os
+
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+ACCEPTED_FILE_EXTENSIONS = ['.pdf', '.doc', '.docx']
+
+
+def validate_file_type(value):
+    name, ext = os.path.splitext(value.name)
+    if ext not in ACCEPTED_FILE_EXTENSIONS:
+        raise ValidationError(
+            _('%(value)s is not %(extensions)s file type!'),
+            params={
+                'value': value.name,
+                'extensions': '/'.join(ACCEPTED_FILE_EXTENSIONS)
+            },
+        )
 
 
 class CVUploadForm(forms.Form):
     cv_file = forms.FileField(
         label='Your CV file',
         required=True,
-        validators=[],
-        widget=forms.FileInput(attrs={'class': 'form-control-file'})
+        validators=[validate_file_type],
+        widget=forms.FileInput(attrs={'class': 'form-control-file'}),
+        help_text=f'Only {"/".join(ACCEPTED_FILE_EXTENSIONS)} could be accepted'
     )
